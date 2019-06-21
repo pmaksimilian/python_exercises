ddea40ea3ab6cc75e92c8f4eb33a93aedb5b10dd
Maksimilian Pavsic <pavsicmaks@gmail.com>
2019-06-21 11:49:58 +0200

    Initial commit.

---
+++ if_else/mood_checker.py
@@ -0,0 +1,14 @@
+mood = input("What mood are you in? ")
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times!")
+elif mood == "sad":
+    print("Hey, cheer up!")
+elif mood == "excited":
+    print("And you just can't hide it!")
+elif mood == "relaxed":
+    print("Did you smoke something?")
+else:
+    print("Hm, this is new. So far I only learned about happy, nervous, sad, excited and relaxed.")
